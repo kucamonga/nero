27cd192bbf42b327240d5b8befd4cc993395b7f5
kucamonga <segatto800@gmai.com>
2026-03-29 20:41:37 -0300

    feat: primeira versão

--- functions/log-event.js
+++
@@ -1,1 +0,0 @@
-exports.handler = async (event) => { const data = JSON.parse(event.body); console.log("Evento registrado:", data.msg); return { statusCode: 200, body: JSON.stringify({status:"ok"}) }; };
